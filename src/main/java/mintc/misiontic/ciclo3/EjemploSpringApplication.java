d812592485a1bfada81bb04ed364993e4978314c
camolano <cmolanom@unicartagena.edu.co>
2026-05-25 19:29:42 -0500

    DESARROLLO DEL PROYECTO DE DESARROLLO DE SOFTWARE CON SPRINT BOOT 4 SEMESTRE PROFESOR JHON CARLOS ARRIETA ARRIETA

---
+++ src/main/java/mintc/misiontic/ciclo3/EjemploSpringApplication.java
@@ -0,0 +1,13 @@
+package mintc.misiontic.ciclo3;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EjemploSpringApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EjemploSpringApplication.class, args);
+	}
+
+}
---
+++ src/test/java/mintc/misiontic/ciclo3/EjemploSpringApplicationTests.java
@@ -0,0 +1,13 @@
+package mintc.misiontic.ciclo3;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EjemploSpringApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
